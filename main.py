d6edfc10dc477ebf3f11bf6055cad48d7eeabbc9
jameel-max <mstfyaysht384@gmail.com>
2025-04-29 02:09:17 +0300

    تعديلات على تصميم لوحة التحكم

--- main.py
+++ main.py
@@ -75,5 +75,6 @@ def admin():
 
     return render_template('admin.html', appointments=appointments)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=10000)
